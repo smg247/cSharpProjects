14596d552f41d41e018a09a6aa208d7f282d9b26
Stephen Goeddel <sgoeddel247@gmail.com>
2016-05-28 23:45:10 -0400

    Simple fibonaci program.

---
+++ Sample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sample
+{
+	class Fibonaci
+	{
+		private static int MAX_TO_FIND = 50;
+
+		private long[] results = new long[MAX_TO_FIND];
+
+		public static void Main (string[] args)
+		{
+			Fibonaci fibonaci = new Fibonaci ();
+			Console.WriteLine ("Fibonaci");
+			for (int i = 0; i < MAX_TO_FIND; i++) {
+				Console.Write ("of " + i + ": ");
+				long result = fibonaci.fib (i);
+				Console.WriteLine (result);
+			}
+		}
+
+		private long fib(int n)
+		{
+			if (n == 0 || n == 1) {
+				results [n] = n;
+				return n;
+			} else {
+				long result = results [n - 2] + results [n - 1];
+				results [n] = result;
+				return result;
+			}
+		}
+	}
+}
